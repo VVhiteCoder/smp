e3710506dd21084d2d2b4c99f48cae14c5c5511f
Vitalie <demonvv@gmail.com>
2013-07-26 14:22:48 +0300

    show file and dir list, wolking through dirs

--- main.py
+++ main.py
@@ -4,72 +4,89 @@ import os
 from conf import *
 
 
+class Thumbs():
+    pass
+
+
 class GenerateFileList():
     def __init__(self):
         self.photo_dirs = {}
         self.file_list = {}
 
     def check_extension(self, f=''):
+        """
+        check if extension of 'f' is listed in conf file
+        """
         for extension in PHOTO_EXTENSIONS:
             (name, ext) = os.path.splitext(f)
             if extension.lower() in ext or extension.upper() in ext:
                 return True
 
     def check_include_photo(self, path=''):
+        """
+        check if folder contain files with permitted extensions
+        """
         # iterate over dir's files
         for f in os.listdir(path):
             # check file is photo
             self.check_extension(os.path.join(path, f))
 
     def get_file_list(self, path=''):
+        """
+        return dict of permitted files of a dir - 'path'
+        """
         for f in os.listdir(path):
             (name, ext) = os.path.splitext(f)
-            if os.path.isfile(os.path.join(path, f)) and self.check_extension():
+            if os.path.isfile(os.path.join(path, f)) and self.check_extension(f):
                 self.file_list[f] = os.path.join(path, f)
-        return self.file_list
+        if len(self.file_list):
+            return self.file_list
+        else:
+            return None
 
     def walk(self, path=''):
+        """
+        return dict of dirs from 'path' that contain permited files
+        """
         for ff in os.listdir(path):
             if os.path.isdir(os.path.join(path, ff)) and not ff[0] == '.':
                 self.photo_dirs[ff] = os.path.join(path, ff)
-        return self.photo_dirs
+        if len(self.photo_dirs):
+            return self.photo_dirs
+        else:
+            return None
 
     def check_integrity(self, path=''):
+        """
+        check if path is not root dir of paths listed in conf file
+        """
         for sh_path in PATHS:
             if not sh_path in path:
                 return False
         return True
 
     def ls(self, path=''):
+        """
+        return verified dirs and files of 'path'
+        """
         if self.check_integrity(path):
-            pass
+            return [self.walk(path), self.get_file_list(path)]
         else:
             return 'error'
-
-
-    # def walk(self, path=''):
-    #     for r, d, f in os.walk(path):
-    #         self.dirs.append(r)
-    #     for d in self.dirs:
-    #         if self.check_include_photo(d):
-    #             (path, dirname) = os.path.split(d)
-    #             if not dirname[0] == '.':
-    #                 self.photo_dirs[dirname] = d
-    #     return self.photo_dirs
-
-    # def get_dirlist(self, path=''):
-    #     pass
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         gfl = GenerateFileList()
-        file_name = self.get_argument("q", None)
-        if file_name:
-            dir_list = gfl.walk(file_name)
+        path = self.get_argument("q", None)
+        if path:
+            (dir_list, file_list) = gfl.ls(path)
         else:
-            dir_list = gfl.walk(PATHS[0])
-        self.render("templates/base.html", dir_list=dir_list, title="smp")
+            (dir_list, file_list) = gfl.ls(PATHS[0])
+        self.render("templates/base.html",
+                    dir_list=dir_list,
+                    file_list=file_list,
+                    title="smp")
 
 
 app = tornado.web.Application(
